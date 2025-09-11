04e7bac11f86deae05fe04d79ca32a0dfb0348a2
Tyler Neill <tyler.g.neill@gmail.com>
2025-09-10 21:02:52 -0400

    chore: separate out regexes

---
+++ utils/transforms/xml/tei_builder_regexes.py
@@ -0,0 +1,12 @@
+import re
+
+SECTION_RE = re.compile(r"^{([^}]+)}\s*$")
+LOCATION_VERSE_RE = re.compile(r"^\[([^\]]+?)\]\t*(.*)$")  # [label] +/- tabbed verse content
+VERSE_NUM_RE = re.compile(r"^\s*([0-9]+(?:[.,][0-9]+)*)\s*([a-z]{1,4})?\s*$", re.I)
+PAGE_RE = re.compile(r"^<(\d+)>$")  # <page>
+PAGE_LINE_RE = re.compile(r"^<(\d+),(\d+)>$")  # <page,line>
+ADDITIONAL_STRUCTURE_NOTE_RE = re.compile(r"^<[^\n>]+>$")  # other <...>
+VERSE_MARKER_RE = re.compile(r"\|\| ([^|]{1,20}) \|\|(?: |$)")
+VERSE_BACK_BOUNDARY_RE = re.compile(r"\|\|? (?![^|]{1,20} \|\|)")
+CLOSE_L_RE = re.compile(r"\|\|?(?:[ \n]|$)")
+HYPHEN_EOL_RE = re.compile(r"-\s*$")  # tweak later if you need fancy hyphens
