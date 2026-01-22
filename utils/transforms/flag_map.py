a4401dac2ffb6e4667ea711f89b3457675dcedb0
Tyler Neill <tyler.g.neill@gmail.com>
2026-01-22 14:44:25 -0500

    chore: add entry to flag_map

--- utils/transforms/flag_map.py
+++ utils/transforms/flag_map.py
@@ -1,6 +1,7 @@
 flag_map = {
     "bANa_kAdambarI": '--line-by-line',
     "kumArilabhaTTa_zlokavArtika": '--verse-only',
+    "vAkyapadIyaprameyasaMgraha": '--line-by-line --extra-space-after-location',
     "zukasaptati_s": '--line-by-line --extra-space-after-location',
     "zukasaptati_o": '--line-by-line --extra-space-after-location',
 }
