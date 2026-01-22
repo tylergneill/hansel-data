df089ba56eb3cb60fbae9317c5e1e274e04ff8fe
Tyler Neill <tyler.g.neill@gmail.com>
2026-01-22 14:40:08 -0500

    refactor (scout): pull out shared flag_map, tidy up globals and imports

---
+++ utils/transforms/flag_map.py
@@ -0,0 +1,6 @@
+flag_map = {
+    "bANa_kAdambarI": '--line-by-line',
+    "kumArilabhaTTa_zlokavArtika": '--verse-only',
+    "zukasaptati_s": '--line-by-line --extra-space-after-location',
+    "zukasaptati_o": '--line-by-line --extra-space-after-location',
+}
