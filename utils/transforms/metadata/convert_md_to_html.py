16e335f2fe826d1a0b279c36b99ce4fd6c9deb0e
Tyler Neill <tyler.g.neill@gmail.com>
2026-01-23 14:22:22 -0500

    fix: include Additional Notes

--- utils/transforms/metadata/convert_md_to_html.py
+++ utils/transforms/metadata/convert_md_to_html.py
@@ -36,6 +36,7 @@ FIELDS_TO_KEEP = [
     'File Creation Method',
     'Text Type',
     'Word Division Style',
+    'Additional Notes',
     'Original Submission Last Updated',
     'Text Last Updated',
     'Metadata Last Updated',
