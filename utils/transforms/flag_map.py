fe590c4268774b291e58121b9a0d1eee79b1cd64
Tyler Neill <tyler.g.neill@gmail.com>
2026-02-24 15:36:19 -0500

    chore: regen with corrected flag map

--- utils/transforms/flag_map.py
+++ utils/transforms/flag_map.py
@@ -3,5 +3,5 @@ flag_map = {
     "vAkyapadIyaprameyasaMgraha": '--line-by-line --extra-space-after-location',
     "zukasaptati_s": '--line-by-line --extra-space-after-location',
     "zukasaptati_o": '--line-by-line --extra-space-after-location',
-    "prabodhacandrodaya": '--drama',
+    "kRSNamizra_prabodhacandrodaya": '--drama',
 }
